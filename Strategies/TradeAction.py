507ed5864db455d89eaac8b9db3813f2ec120bf5
Steven Kazmierkiewicz <stevekaz@gmail.com>
2018-03-09 15:12:32 -0500

    Removed 'shares' attribute from TradeAction, fixed test

--- Strategies/TradeAction.py
+++ Strategies/TradeAction.py
@@ -3,28 +3,15 @@ class TradeAction():
     SELL = "SELL"
     HOLD = "HOLD"
     
-    def __init__(self, time, action, shares=0):
+    def __init__(self, time, action):
         self.time = time
         self.action = action
-        
-        #not sure if we need to have this logic in this class or in the
-        #result class
-        self.shares = shares
     
     def __str__(self):
-        ret_str = str(self.time) + ": " + self.action
-        
-        if self.action != self.HOLD:
-            ret_str += " " + str(self.shares) + " shares"
-        
-        return ret_str
+        return str(self.time) + ": " + self.action
     
     def get_time(self):
         return self.time
     
     def get_action(self):
         return self.action
-    
-    def get_shares(self):
-        return self.shares
-    
--- tests/test_TradeAction.py
+++ tests/test_TradeAction.py
@@ -1,15 +1,15 @@
 from Strategies.TradeAction import TradeAction
 
 test_trades = [
-    [1, TradeAction.BUY, 20],
-    [2, TradeAction.SELL, 50],
-    [3, TradeAction.HOLD, 0]
+    [1, TradeAction.BUY],
+    [2, TradeAction.SELL],
+    [3, TradeAction.HOLD]
     ]
 
 trades = []
 
 for trade in test_trades:
-    trades.append(TradeAction(trade[0], trade[1], trade[2]))
+    trades.append(TradeAction(trade[0], trade[1]))
     
 for action in trades:
     print action
