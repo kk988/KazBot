aae85d1f87fb26956ee270a9e9cca5e8b8fff5f5
Steven Kazmierkiewicz <stevekaz@gmail.com>
2018-03-13 22:23:26 -0400

    Made changes so Backtest works with StochRSI, Trader, Simulation, Account

--- Strategies/StrategyManager.py
+++ Strategies/StrategyManager.py
@@ -4,7 +4,11 @@ Created on Feb 11, 2018
 @author: kristakazmierkiewicz
 '''
 from StochRSI import StochRSI
+from Trader import Trader
 
 def run_strategy(strategy, data, **kwargs):
     if strategy == 'StochRSI':
-        return StochRSI(data, **kwargs)
+        if 'n_period' in kwargs:
+            return Trader(StochRSI(data, kwargs['n_period']).get_stoch_rsi_vals()).get_actions()
+        else:
+            raise RuntimeError("StochRSI Strategy requires a specific n_period.")
--- lib/Backtest.py
+++ lib/Backtest.py
@@ -1,5 +1,8 @@
 from Strategies.StrategyManager import run_strategy
-from Strategies import Trader  
+from Strategies.Trader import Trader  
+from Strategies.Account import Account
+from Strategies.Simulation import Simulation
+from exchanges.gdax import CandleList
 import datetime
 import iso8601
 import math
@@ -10,22 +13,25 @@ import time
 #test_input=pull_candles( datetime(2018, 2, 1, 0, 0, 0), datetime(2018, 2, 2, 0, 0, 0), 300)
 
 class Backtest():
-    def __init__(self, start, end, granularity, strategy, **kwargs):
+    def __init__(self, start, end, granularity, strategy, share_trade_ratio, **kwargs):
         # Pull data
         # Not sure if I'll need to save this.
-        self.data = pull_candles(iso8601.parse_date(start), iso8601.parse_date(end), granularity)
-        if not self.data:
+        data = pull_candles(iso8601.parse_date(start), iso8601.parse_date(end), granularity)
+        if not data:
             # something went wrong. create error and exit
             raise RuntimeError('Something went wrong pulling data for backtest') 
-            
-        #run strategy
-        self.strategy = run_strategy(strategy, self.data, **kwargs)
         
-        # use strategy output with exchange rules
-        # Call trader to figure out buys/sells
-        self.trading_calls = Trader(strategy)
+        self.candle_list = CandleList(data, granularity)
+        #run strategy, returns trading calls
         
-        #self.results = generate_result_summary(trading_calls)
+        print "***************kwargs:\n", kwargs
+        self.strategy = run_strategy(strategy, self.candle_list, **kwargs)
+        
+        # Run simulation on account.
+        account = Account(5000)
+        self.sim = Simulation(self.candle_list, self.strategy, account, share_trade_ratio)
+        
+        generate_result_summary(self.sim, granularity, **kwargs)
     
 def pull_candles( start, end, granularity):
     # Get total number of candles
@@ -57,3 +63,24 @@ def pull_candles( start, end, granularity):
         curr_start = curr_end
             
     return sorted(candle_data)
+
+def generate_result_summary(sim, granularity, **kwargs):
+    print "\n--------------------"
+    print "Backtest Results"
+    print "--------------------"
+    print "\nStart Time:", sim.get_start_datetime()
+    print "End Time:", sim.get_end_datetime()
+    print "\nStart Value:", sim.get_start_value()
+    print "End Value:", sim.get_end_value()
+    print "Value Change:", sim.get_value_change()
+    print "\nStart Price:", sim.get_start_price()
+    print "End Price:", sim.get_end_price()
+    print "Price Change:", sim.get_price_change()
+    
+    print "\nGranularity:", granularity
+    if kwargs:
+        for (key, value) in kwargs.iteritems():
+            print ": ".join([str(key), str(value)])
+    print "Value Change Percent:", sim.get_value_change_percent() * 100, "%"
+    print "Price Change Percent:", sim.get_price_change_percent() * 100, "%"
+    print "Trades: ", sim.get_trades()
