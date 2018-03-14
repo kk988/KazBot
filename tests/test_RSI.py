ad5c83392dbcf7ae846a462baed84b7db76bcfd7
Krista Kaz <kristakaz@gmail.com>
2018-03-13 21:51:54 -0700

    made stochrsi a unittest

--- tests/test_RSI.py
+++ tests/test_RSI.py
@@ -9,9 +9,6 @@ def load_comma_delim_list(in_file):
             test_input.append(inline.rstrip().split(","))
     return test_input
 
-def format_list(list_to_change, format_type):
-    return map(format_type, list_to_change)
-
 class TestRSI(unittest.TestCase):
     FILE_DIR = "files/RSI"
     
@@ -19,8 +16,8 @@ class TestRSI(unittest.TestCase):
         test_input = load_comma_delim_list("/".join([self.FILE_DIR,"RSI_init_input.txt"]))
         expected_rsi = load_comma_delim_list("/".join([self.FILE_DIR,"RSI_init_expected.txt"]))
         
-        test_input = [ format_list(x, float) for x in test_input ]
-        expected_rsi = [ format_list(x, float) for x in expected_rsi ] 
+        test_input = [ map(float, x) for x in test_input ]
+        expected_rsi = [ map(float, x) for x in expected_rsi ] 
     
         # Setup Candles (graularity of input is 900 sec)
         test_candles = CandleList(test_input, 900)
@@ -37,7 +34,7 @@ class TestRSI(unittest.TestCase):
         expected_outs = load_comma_delim_list("/".join([self.FILE_DIR, "calc_rsi_expected.txt"])).pop()
         
         for x in range(len(test_inputs)):
-            (avg_gains, avg_losses) = format_list(test_inputs[x], float)
+            (avg_gains, avg_losses) = map(float, test_inputs[x])
             rsi_val = RSI.calc_rsi(avg_gains, avg_losses)
             self.assertEqual(round(rsi_val, 8), float(expected_outs[x]))
         
