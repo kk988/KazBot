824a7066226066c0203bef4f639dd2af31b8889b
Krista Kaz <kristakaz@gmail.com>
2018-03-13 20:05:19 -0700

    more changes in order to make test_RSI better

--- Strategies/RSI.py
+++ Strategies/RSI.py
@@ -12,21 +12,21 @@ class RSI():
         avg_gains = sum(gains) / n_periods
         avg_losses = sum(losses) / n_periods
         
-        self.vals.append([candle_list[n_periods-1].get_start_time() , self.calc_rsi(avg_gains, avg_losses)])
+        self.vals.append([candle_list[n_periods-1].get_start_time() , calc_rsi(avg_gains, avg_losses)])
 
         for i in range(n_periods, len(candle_list)):
             (cur_gain,cur_loss) = pull_gains_and_losses([candle_list[i]])
             avg_gains = (avg_gains * (n_periods-1) + cur_gain[0]) / n_periods
             avg_losses = (avg_losses * (n_periods-1) + cur_loss[0]) / n_periods
             
-            self.vals.append([candle_list[i].get_start_time(), self.calc_rsi(avg_gains, avg_losses)])
+            self.vals.append([candle_list[i].get_start_time(), calc_rsi(avg_gains, avg_losses)])
             
     def get_calculated_rsi_vals(self):
         return self.vals
     
-    def calc_rsi(self, avg_gains, avg_losses):
-        RS = avg_gains/avg_losses
-        return 100 - (100/(1+RS))
+def calc_rsi(avg_gains, avg_losses):
+    RS = avg_gains/avg_losses
+    return (100 - (100/(1+RS)))
         
     
 def pull_gains_and_losses(candle_list):
--- tests/test_RSI.py
+++ tests/test_RSI.py
@@ -6,14 +6,21 @@ def load_comma_delim_list(in_file):
     test_input = list()
     with open(in_file) as f:
         for inline in f:
-            test_input.append(map(float, inline.rstrip().split(",")))
+            test_input.append(inline.rstrip().split(","))
     return test_input
 
+def format_list(list_to_change, format_type):
+    return map(format_type, list_to_change)
+
 class TestRSI(unittest.TestCase):
+    FILE_DIR = "files/RSI"
     
-    def testCalculations(self):
-        test_input = load_comma_delim_list("files/RSI_input.txt")
-        expected_rsi = load_comma_delim_list("files/RSI_expected.txt")
+    def test_init(self):
+        test_input = load_comma_delim_list("/".join([self.FILE_DIR,"RSI_init_input.txt"]))
+        expected_rsi = load_comma_delim_list("/".join([self.FILE_DIR,"RSI_init_expected.txt"]))
+        
+        test_input = [ format_list(x, float) for x in test_input ]
+        expected_rsi = [ format_list(x, float) for x in expected_rsi ] 
     
         # Setup Candles (graularity of input is 900 sec)
         test_candles = CandleList(test_input, 900)
@@ -24,6 +31,17 @@ class TestRSI(unittest.TestCase):
         rnd_my_vals = [[x[0], round(x[1], 8)] for x in my_vals]
         
         self.assertEqual(rnd_my_vals, expected_rsi)
+        
+    def test_calc_rsi(self):
+        test_inputs = load_comma_delim_list("/".join([self.FILE_DIR, "calc_rsi_input.txt"]))
+        expected_outs = load_comma_delim_list("/".join([self.FILE_DIR, "calc_rsi_expected.txt"])).pop()
+        
+        for x in range(len(test_inputs)):
+            (avg_gains, avg_losses) = format_list(test_inputs[x], float)
+            rsi_val = RSI.calc_rsi(avg_gains, avg_losses)
+            self.assertEqual(round(rsi_val, 8), float(expected_outs[x]))
+        
+    
 
 if __name__ == '__main__':
     unittest.main()
