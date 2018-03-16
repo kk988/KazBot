f3ab0f54abfeab0b3c6fbb38f981c7413d8f34ea
Krista Kaz <kristakaz@gmail.com>
2018-03-15 22:08:06 -0700

    made little changes and added timestamps to buy sell as well as price.

--- tests/run_Backtest.py
+++ tests/run_Backtest.py
@@ -1,7 +1,7 @@
 from lib.Backtest import Backtest
 
-btest1 = Backtest("2018-1-1", "2018-2-1", 300, "StochRSI", 1, n_period=14)
-btest2 = Backtest("2017-10-1", "2018-2-1", 900, "StochRSI", 1, n_period=14)
+#btest1 = Backtest("2018-1-1", "2018-2-1", 300, "StochRSI", 1, n_period=14)
+btest2 = Backtest("2018-01-01", "2018-2-1", 900, "StochRSI", 1, n_period=15)
 
-btest1.generate_result_summary()
+#btest1.generate_result_summary()
 btest2.generate_result_summary()
